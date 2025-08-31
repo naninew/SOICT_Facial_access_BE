6066c305eaf6ef507a686096700f2646e9793f2a
Đỗ Xuân Quý <153648226+naninew@users.noreply.github.com>
2025-09-01 01:18:23 +0700

    Update my work on API samples

---
+++ API_sample/WebSockets/WebSockets/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Graph.Models;
+
+var builder= WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+var app= builder.Build();
+
+var webSocketOptions= new WebSocketOptions
+{
+    KeepAliveInterval= TimeSpan.FromSeconds(120),
+    //ReceiveBufferSize= 4 * 1024
+};
+
+app.UseWebSockets(webSocketOptions);
+
+app.UseDefaultFiles();  
+app.UseStaticFiles();
+
+app.MapControllers();
+app.Run();
